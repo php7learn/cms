a630c912240e759ece02a54e6e7555b8203af9ad
程志雷 <chengzl@shengruan.com>
2019-03-05 20:23:33 +0800

    test

--- api2.php
+++ api2.php
@@ -9,6 +9,9 @@ class apiCtrl extends base
         $this->setsubtemplates('index');
         $act = isset($_GET['act']) ? $_GET['act'] : '';
         switch ($act) {
+            case 'login':
+                $this->login();
+                break;
             case 'setadmin':
                 $this->setadmin();
                 break;
@@ -109,7 +112,61 @@ class apiCtrl extends base
         var_dump($type);
     }
 
-
+    private function login(){
+        //获取数据并记录
+        if(isset($_GET['code'])){
+            $code = $_GET['code'];//登录临时凭证
+        }else{
+            echo '{"result":"2","message":"code null"}';
+            exit();
+        }
+        require_once 'classes/index.class.php';
+        $obj = new indexClass();
+        //保存附加信息
+        /* $systeminfo = isset($_GET['systeminfo'])?addslashes($_GET['systeminfo']):"";
+        $network = isset($_GET['network'])?addslashes($_GET['network']):"";
+        $refer = isset($_SERVER["HTTP_REFERER"])?addslashes($_SERVER["HTTP_REFERER"]):'';
+          */
+        //登录凭证校验
+        /**
+         在不满足UnionID下发条件的情况下，返回参数
+         参数	说明
+         openid	用户唯一标识
+         session_key	会话密钥
+         在满足UnionID下发条件的情况下，返回参数
+         参数	说明
+         openid	用户唯一标识
+         session_key	会话密钥
+         unionid	用户在开放平台的唯一标识符
+         **/
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.APPID.'&secret='.APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
+        $result = file_get_contents($url);
+        $result = json_decode($result);
+        //unionid
+        if(isset($result->openid)){
+            if(isset($result->unionid)){
+                $unionid = $result->unionid;
+                $member_id = $obj->getonerow($unionid, "edc_member_bind_unionid", "bind_id");
+                $member_id = array("member_id"=>573);
+                if($member_id){
+                    $member_id = $member_id['member_id'];
+                    $obj->execute("insert into edc_miniprogram_login_log(member_id,unionid,openid,time) values (".$member_id.",'".$unionid."','".$result->openid."',".time().")");
+                    $key = Common::get_authcode_key ();
+                    $tokenId = Common::authcode ($unionid.'@@'.$member_id. '@@' .$result->openid . '@@' . time (), $operation = 'ENCODE', $key, $expiry = 0);
+                    $tokenId=str_replace('+','-',$tokenId);
+                    $tokenId=str_replace('/','_',$tokenId);
+                    $tokenId=str_replace('=','*',$tokenId);
+                    echo json_encode(array("result"=>"0","token"=>$tokenId));
+                }else{
+                    echo '{"result":"4","message":"No userid"}';
+                }
+            }else{
+                echo '{"result":"3","message":"No unionid"}';
+            }
+        }else{
+            echo '{"result":"1","message":"'.$result->errcode.'"}';
+        }
+    }
 
 
 
