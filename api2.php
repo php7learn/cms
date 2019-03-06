f4f3490d93426b9d303613dec17a4b5375b5f9cc
程志雷 <chengzl@shengruan.com>
2019-03-06 18:34:14 +0800

    test

--- api2.php
+++ api2.php
@@ -3,9 +3,10 @@ require "config.php";
 
 class apiCtrl extends base
 {
-
+    var $key;
     public function main()
     {
+        $this->key = APPID;
         $this->setsubtemplates('index');
         $act = isset($_GET['act']) ? $_GET['act'] : '';
         switch ($act) {
@@ -15,23 +16,23 @@ class apiCtrl extends base
             case 'setadmin':
                 $this->setadmin();
                 break;
-            // 获取banner
+            //获取banner
             case 'get_banner':
                 $this->get_banner();
                 break;
-            // 获取分类
+            //获取分类
             case 'get_catelog':
                 $this->get_catelog();
                 break;
-            // 获取获取商品列表
+            //获取获取商品列表
             case 'get_goods_list':
                 $this->get_goods_list();
                 break;
-            // 获取指定商品信息
+            //获取指定商品信息
             case 'get_goods_info':
                 $this->get_goods_info();
                 break;
-            // 地址管理
+            //地址管理
             case 'get_address':
                 $this->get_address();
                 break;
@@ -42,98 +43,106 @@ class apiCtrl extends base
 
     function index()
     {
-        $openid = $this->check_user();
-        $u_id = $openid[2];
-        
         echo "hello";
     }
-    
-    // banner接口
-    private function get_banner()
-    {
+
+    //banner接口
+    private function get_banner(){
         require_once 'classes/index.class.php';
-        
+
         $obj = new indexClass();
-        $res = $obj->select_one('shop_goods_main', 1);
-        // print_r($res);
-        $list_banner = array(
-            '1.jpg',
-            '2.jpg',
-            '3.jpg'
-        );
-        echo json_encode($list_banner);
-    }
-    
-    // 分类接口 2级
-    private function get_catelog()
-    {
-        $cat = array(
-            '生鲜' => array(
-                '土豆',
-                '蔬菜'
-            ),
-            '家电' => array(
-                '洗衣机',
-                '电冰箱'
-            ),
-            '厨具' => array(
-                '刀',
-                '砧板'
-            )
-        );
-        echo json_encode($cat);
-        // print_r($cat);
-    }
-    
-    // 获取列表
-    private function get_goods_list()
-    {
-        require_once 'classes/index.class.php';
-        $obj = new indexClass();
-        
-        require_once 'classes/String.class.php';
-        $catelog = intval(string::strip_html_tags_new($weight = isset($_GET['catelog']) ? (int) $_GET['catelog'] : 0));
-        if ($catelog == 0) {
-            $list = $obj->select("shop_goods_main");
-            echo json_encode($list);
-        } else {
-            $list = $obj->select("shop_goods_main", "catelog=>$catelog");
-            echo json_encode($list);
-        }
+        $res = $obj->select_one('shop_goods_main',1);
+//        print_r($res);
+        $list_banner = array('1.jpg','2.jpg','3.jpg');
+        echo  json_encode($list_banner);
     }
 
-    private function get_goods_info()
-    {
+
+    //分类接口 2级
+    private function get_catelog(){
+        $cat = array('生鲜'=>array('土豆','蔬菜'),'家电'=>array('洗衣机','电冰箱'),'厨具'=>array('刀','砧板'));
+        echo json_encode($cat);
+//        print_r($cat);
+    }
+
+    //获取列表
+    private function get_goods_list(){
         require_once 'classes/index.class.php';
         $obj = new indexClass();
-        
+
         require_once 'classes/String.class.php';
-        $id = intval(string::strip_html_tags_new($weight = isset($_GET['id']) ? (int) $_GET['id'] : 0));
-        if ($id <= 0) {
-            echo '{"resutl":1,"msg":"输入错误"}';
-            exit();
+        $catelog = intval(string::strip_html_tags_new ( $weight = isset($_GET['catelog']) ? (int)$_GET['catelog'] : 0 ));
+        if($catelog == 0){
+            $list = $obj->select("shop_goods_main");
+            echo json_encode( $list );
+        }else{
+            $list = $obj->get_array("select * from shop_goods_main where catelog=$catelog");
+            echo json_encode( $list );
+        }
+
+    }
+
+    private function get_goods_info(){
+        require_once 'classes/index.class.php';
+        $obj = new indexClass();
+
+        require_once 'classes/String.class.php';
+        $id = intval(string::strip_html_tags_new ( $weight = isset($_GET['id']) ? (int)$_GET['id'] : 0 ));
+        if($id <= 0){
+            echo '{"resutl":1,"msg":"输入错误"}';exit();
         }
         $info = $obj->get_array("select * from shop_goods_main as t1 ,shop_goods_desc as t2 where t1.id=t2.goods_id and t1.id=$id");
-        echo json_encode($info);
+        echo json_encode( $info );
     }
 
-    private function get_address()
-    {
+
+    private function get_address(){
+        //$this->check_user();
         require_once 'classes/index.class.php';
         $obj = new indexClass();
         require_once 'classes/String.class.php';
-        
-        $type = intval(string::strip_html_tags_new($weight = isset($_GET['type']) ? (int) $_GET['type'] : 0));
-        $addr = string::strip_html_tags_new($weight = isset($_POST['address']) ? $_POST['address'] : '');
-        $phone = string::strip_html_tags_new($weight = isset($_POST['phone']) ? $_POST['phone'] : '');
-        $name = string::strip_html_tags_new($weight = isset($_POST['name']) ? $_POST['name'] : '');
-        $user_id = string::strip_html_tags_new($weight = isset($_POST['user_id']) ? (int) $_POST['user_id'] : '');
-        $post_code = string::strip_html_tags_new($weight = isset($_POST['post_code']) ? $_POST['post_code'] : '');
-        if ($type == 0) {
-            // $res = $obj->insert_sql("insert into ");
+
+        $type =intval(string::strip_html_tags_new ( $weight = isset($_GET['type']) ? (int)$_GET['type'] : 0 ));
+        $addr = string::strip_html_tags_new ( $weight = isset($_POST['address']) ? $_POST['address'] : '' );
+        $phone = string::strip_html_tags_new ( $weight = isset($_POST['phone']) ? $_POST['phone'] : '' );
+        $name = string::strip_html_tags_new ( $weight = isset($_POST['name']) ? $_POST['name'] : '' );
+        $user_id = intval(string::strip_html_tags_new ( $weight = isset($_POST['user_id']) ? (int)$_POST['user_id'] : '' ));
+        $post_code = string::strip_html_tags_new ( $weight = isset($_POST['post_code']) ? $_POST['post_code'] : '' );
+        $address_id = intval(string::strip_html_tags_new ( $weight = isset($_GET['address_id']) ? (int)$_GET['address_id'] : '' ));
+        $time = time();
+        if(empty( $phone ) && $type != 2){
+            echo '{"resutl":1,"msg":"手机号码为空"}';exit();
         }
-        var_dump($type);
+        if($type == 0){
+            $res = $obj->insert_sql("insert into shop_user_address(name,address,user_id,post_code,phone,create_time) values('$name','$addr','$user_id','$post_code','$phone','$time')");
+            if($res){
+                echo '{"resutl":"'.$res.'","msg":"新增地址成功"}';exit();
+            }else{
+                echo '{"resutl":1,"msg":"新增地址失败"}';exit();
+            }
+
+        }
+        if($type == 1){
+            $res = $obj->update_sql("update shop_user_address set name='$name',address='$addr',phone='$phone',post_code='$post_code',update_time='$time' where address_id='$address_id' and user_id='$user_id'");
+            if($res){
+                echo '{"resutl":0,"msg":"更新地址成功"}';exit();
+            }else{
+                echo '{"resutl":1,"msg":"更新地址失败"}';exit();
+            }
+        }
+
+        if($type == 2){
+            $res = $obj->delete("shop_user_address","address_id=$address_id");
+            if($res){
+                echo '{"resutl":0,"msg":"删除地址成功"}';exit();
+            }else{
+                echo '{"resutl":1,"msg":"删除地址失败"}';exit();
+            }
+        }
+
+//        var_dump($type);
     }
+
 
     private function login()
     {
@@ -161,7 +170,7 @@ class apiCtrl extends base
         $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' . APPID . '&secret=' . SECRET . '&js_code=' . $code . '&grant_type=authorization_code';
         $result = file_get_contents($url);
         $result = json_decode($result);
-        
+    
         $userid = 0;
         if (isset($result->openid)) {
             $ip = $this->get_ip();
@@ -177,90 +186,94 @@ class apiCtrl extends base
                 $obj->insert_sql("insert into shop_user_main(openid) values ('".$result->openid."')");
                 $userid = mysqli_insert_id($obj->link);
             }
-            
+    
             // 返回tokenid
             require_once ('classes/des1.class.php');
-            
+    
             $encrypt = new des1($this->key);
             $tokenId = $encrypt->encrypt($result->openid . '@@' . time() . '@@' . $userid);
             $tokenId = str_replace('+', '-', $tokenId);
             $tokenId = str_replace('/', '_', $tokenId);
             $tokenId = str_replace('=', '*', $tokenId);
             header('Content-type: application/json');
-            
+    
             $expires = time() + 3600;
             echo '{"result":"0","tokenid":"' . $tokenId . '","userid":"' . $userid . '","expires":"' . $expires . '000"}';
         } else {
             echo '{"result":"1","message":"' . $result->errcode . '"}';
         }
     }
-	//获取ip
-	function get_ip(){
-		$ip=false;
-		if(!empty($_SERVER["HTTP_CLIENT_IP"])){
-			$ip = $_SERVER["HTTP_CLIENT_IP"];
-		}
-		if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-			$ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
-			if ($ip){
-				array_unshift($ips, $ip); $ip = FALSE;
-			}
-			for ($i = 0; $i < count($ips); $i++){
-				if(strpos($ips[$i],'know') > 0){// unknow
-					continue;
-				}
-		    	if (!eregi ("^(10|172\.16|192\.168)\.", $ips[$i])){
-		        	$ip = $ips[$i];
-		            break;
-		    	}
-			}
-		}
-		return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
-	}
-	//校验用户
-	function check_user(){
-		$tokenid = $_GET['tokenid'];
-		if ($tokenid==""||$tokenid=="undefined") {
-			echo '{"result":-1, "message":"tokenid无效"}';
-			exit ();
-		}
-		
-		require_once ('classes/des1.class.php');
-			
-		$encrypt = new des1($this->key);
-		$tokenid=str_replace('-','+',$tokenid);
+    //获取ip
+    function get_ip(){
+        $ip=false;
+        if(!empty($_SERVER["HTTP_CLIENT_IP"])){
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        }
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+            if ($ip){
+                array_unshift($ips, $ip); $ip = FALSE;
+            }
+            for ($i = 0; $i < count($ips); $i++){
+                if(strpos($ips[$i],'know') > 0){// unknow
+                    continue;
+                }
+                if (!eregi ("^(10|172\.16|192\.168)\.", $ips[$i])){
+                    $ip = $ips[$i];
+                    break;
+                }
+            }
+        }
+        return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+    }
+    //校验用户
+    function check_user(){
+        $tokenid = $_GET['tokenid'];
+        if ($tokenid==""||$tokenid=="undefined") {
+            echo '{"result":-1, "message":"tokenid无效"}';
+            exit ();
+        }
+    
+        require_once ('classes/des1.class.php');
+        	
+        $encrypt = new des1($this->key);
+        $tokenid=str_replace('-','+',$tokenid);
         $tokenid=str_replace('_','/',$tokenid);
         $tokenid=str_replace('*','=',$tokenid);
-		
-		$tokenid = $encrypt->decrypt($tokenid);
-		
-		
-		$tokenid_array = explode ( '@@', $tokenid );
-// 		echo time().'--';
-		if(!is_array($tokenid_array)){
-			echo '{"result":-1, "message":"tokenid不合法"}';
-			exit ();
-		}
-		if(!isset($tokenid_array['2'])||$tokenid_array['2']<1){
-			echo '{"result":-1, "message":"id不合法"}';
-			exit ();
-		}
-// 		echo $hours = (time() - $tokenid_array[1])/3600;
-// 		if ($hours > 48) {
-// 			echo '{"result":0, "message":"tokenid过期"}';
-// 			exit ();
-// 		}
-		if (count ( $tokenid_array ) > 0) {
-		    if($tokenid_array[2]==1){
-		        $tokenid_array[2]=508;
-		    }
-		    
-			return $tokenid_array;
-		} else {
-			echo '{"result":-1, "message":"tokenid不合法"}';
-			exit ();
-		}
-	}
+    
+        $tokenid = $encrypt->decrypt($tokenid);
+    
+    
+        $tokenid_array = explode ( '@@', $tokenid );
+        // 		echo time().'--';
+        if(!is_array($tokenid_array)){
+            echo '{"result":-1, "message":"tokenid不合法"}';
+            exit ();
+        }
+        if(!isset($tokenid_array['2'])||$tokenid_array['2']<1){
+            echo '{"result":-1, "message":"id不合法"}';
+            exit ();
+        }
+        // 		echo $hours = (time() - $tokenid_array[1])/3600;
+        // 		if ($hours > 48) {
+        // 			echo '{"result":0, "message":"tokenid过期"}';
+        // 			exit ();
+        // 		}
+        if (count ( $tokenid_array ) > 0) {
+            if($tokenid_array[2]==1){
+                $tokenid_array[2]=508;
+            }
+    
+            return $tokenid_array;
+        } else {
+            echo '{"result":-1, "message":"tokenid不合法"}';
+            exit ();
+        }
+    }
+
+
+
+
 }
 $api = new apiCtrl();
 $api->main();
