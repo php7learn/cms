300ef34752c13f2e2af7b8d22a15d3a830857e3b
程志雷 <chengzl@shengruan.com>
2019-03-23 18:22:50 +0800

    test

--- classes/admin.class.php
+++ classes/admin.class.php
@@ -14,4 +14,31 @@ class adminClass extends dbengine{
 	    $count = $this->selectcount('shop_goods_main','state=1');
 	    return $count;
 	}
+	
+	public function get_users($page = 1,$limit){
+	    //$sql = "select * from shop_goods_main where state=1 order by id asc;";
+	    //$res = $this->link->query($sql);
+	    $offset = ($page-1)*$limit;
+	    $condition=array('where'=>'1','order'=>'user_id asc','offset'=>$offset,'limit'=>$limit);
+	    $res = $this->select('shop_user_main',$condition);
+	    return $res;
+	}
+	
+	public function get_users_count(){
+	    $count = $this->selectcount('shop_user_main');
+	    return $count;
+	}
+	public function get_orders($page = 1,$limit){
+	    //$sql = "select * from shop_goods_main where state=1 order by id asc;";
+	    //$res = $this->link->query($sql);
+	    $offset = ($page-1)*$limit;
+	    $condition=array('where'=>'1','order'=>'order_id asc','offset'=>$offset,'limit'=>$limit);
+	    $res = $this->select('shop_user_order',$condition);
+	    return $res;
+	}
+	
+	public function get_orders_count(){
+	    $count = $this->selectcount('shop_user_order');
+	    return $count;
+	}
 }
